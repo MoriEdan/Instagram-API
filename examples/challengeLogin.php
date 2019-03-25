4ed17b29d65a38aa09f28eabfc903e9a348692d7
Rakita Miljan <rakitamiljan@yahoo.com>
2019-03-25 16:09:40 +0100

    tested version with challenge

---
+++ examples/challengeLogin.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: miki
+ * Date: 2019-03-25
+ * Time: 15:43
+ */
+
+set_time_limit(0);
+date_default_timezone_set('UTC');
+
+require __DIR__.'/../vendor/autoload.php';
+require __DIR__.'/storage/RedisStorage.php';
+require __DIR__.'/storage/util/RedisKeys.php';
+
+/////// CONFIG ///////
+$username = '';
+$password = '';
+$pk = '';
+$debug = true;
+$redisUrl = 'redis://localhost:6379';
+$proxy = '';
+$truncatedDebug = false;
+//////////////////////
+
+$ig = new \InstagramAPI\Instagram($debug, $truncatedDebug,[
+    'storage' => 'custom',
+    'class' => new RedisStorage(),
+    'redis_url' => $redisUrl,
+    'dbtablename' => 'account',
+]);
+
+// need to set some bad proxy so we are thrown on the challenge.
+$ig->setProxy($proxy);
+$ig->client->setVerifySSL(false);
+
+try {
+    $loginResponse = $ig->login($username,$pk, $password);
+
+    if ($loginResponse !== null && $loginResponse->isTwoFactorRequired()) {
+        $twoFactorIdentifier = $loginResponse->getTwoFactorInfo()->getTwoFactorIdentifier();
+
+        echo "TWO-FACTOR";
+    }
+    echo 'User: '.$ig->userLookup('miljan_rakita')->getUser();
+
+} catch (\InstagramAPI\Exception\ChallengeRequiredException $e ) {
+
+    $apiPath = $e->getResponse()->getChallenge()->getApiPath();
+
+    preg_match('#/challenge/([0-9]*?)/(.*?)/#', $apiPath, $reg);
+    $pk = $reg[1];
+    $nonce = $reg[2];
+
+    $challenge = $ig->challenge->getInfo($pk, $nonce);
+
+    $step_name = $challenge->getStepName();
+    $step_data = $challenge->getStepData();
+
+    // The "STDIN" lets you paste the code via terminal for testing.
+    // You should replace this line with the logic you want.
+    // The verification code will be sent by Instagram via SMS.
+    $verifyMethod = trim(fgets(STDIN));
+    $ig->challenge->selectVerifyMethod($verifyMethod);
+
+    $code = trim(fgets(STDIN));
+    $ig->challenge->verify($code);
+
+    echo 'User: '.$ig->userLookup('miljan_rakita')->getUser();
+}
+catch (\Exception $e) {
+    echo 'Something went wrong: '.$e->getMessage()."\n";
+}
---
+++ examples/twoFactorLogin.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: miki
+ * Date: 2019-03-25
+ * Time: 15:25
+ */
+
+set_time_limit(0);
+date_default_timezone_set('UTC');
+
+require __DIR__.'/../vendor/autoload.php';
+require __DIR__.'/storage/RedisStorage.php';
+require __DIR__.'/storage/util/RedisKeys.php';
+
+/////// CONFIG ///////
+$username = '';
+$password = '';
+$pk = '';
+$debug = true;
+$redisUrl = 'redis://localhost:6379';
+$truncatedDebug = false;
+//////////////////////
+
+$ig = new \InstagramAPI\Instagram($debug, $truncatedDebug,[
+    'storage' => 'custom',
+    'class' => new RedisStorage(),
+    'redis_url' => $redisUrl,
+    'dbtablename' => 'account',
+]);
+
+try {
+    $loginResponse = $ig->login($username,$pk, $password);
+
+    if ($loginResponse !== null && $loginResponse->isTwoFactorRequired()) {
+        $twoFactorIdentifier = $loginResponse->getTwoFactorInfo()->getTwoFactorIdentifier();
+
+        // The "STDIN" lets you paste the code via terminal for testing.
+        // You should replace this line with the logic you want.
+        // The verification code will be sent by Instagram via SMS.
+        $verificationCode = trim(fgets(STDIN));
+        $ig->finishTwoFactorLogin($username, $pk, $password, $twoFactorIdentifier, $verificationCode);
+    }
+
+    echo 'User: '.$ig->userLookup('miljan_rakita')->getUser();
+} catch (\Exception $e) {
+    echo 'Something went wrong: '.$e->getMessage()."\n";
+}
